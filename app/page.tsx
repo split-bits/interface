2bb3833028cfdebf65d44f21364f5d6caaa3336b
Ansh <anshsaxena4190@gmail.com>
2024-10-04 21:24:52 +0530

    fix: bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+"use client"
 import React from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 const LandingPage: React.FC = () => {
   return (
